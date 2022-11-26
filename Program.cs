7b4d4128c949457b191d2315ea358cb728aad865
Simon Justesen <simon.justesen@gmail.com>
2022-11-26 22:28:57 +0100

    Hest

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using System;
 using Fediverset.Data;
 using Fediverset.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
